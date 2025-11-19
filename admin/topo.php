9270695610965db63241b6138ef2e6a47c64223f
Rafe-Dev <rafaells.cplays@gmail.com>
2025-11-18 22:20:05 -0300

    adicionando pasta admin juntamente dos arquivos iniciais

---
+++ admin/topo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/administração.css" rel="stylesheet">
+  
+    <title>administração</title>
+</head>
+<body>
+    <header>
+     <div class="center">
+            <div class="logo">
+                <img src="../imagens/logo1.png" width="70px" alt="Logo da loja">
+                <h1>Página administrativa</h1>
+            </div>
+            
+            <nav class="menu">
+                <a href="listausuarios.php"><span class="material-symbols-outlined">LISTA DE USUÁRIOS</span></a>
+                <a href="produtoscadastro.php"><span class="material-symbols-outlined">PRODUTOS CADASTRADOS</span></a>
+            </nav>
+    </header>
+</body>
+</html>
