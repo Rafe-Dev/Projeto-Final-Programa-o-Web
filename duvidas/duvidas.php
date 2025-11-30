a45638b4032c8efb8c405add6842b1b918cfa651
Rafe-Dev <rafaells.cplays@gmail.com>
2025-11-30 10:44:03 -0300

    correção de bugs na página de avaliação e duvidas

--- duvidas/duvidas.php
+++ duvidas/duvidas.php
@@ -2,9 +2,9 @@
 $status_msg = "";
 if (isset($_GET['status'])) {
     if ($_GET['status'] == "ok") {
-        $status_msg = "<p class='msg-sucesso'>Sua avaliação foi enviada com sucesso!</p>";
+        $status_msg = "<p class='msg-sucesso'>Sua dúvida foi enviada com sucesso!</p>";
     } elseif ($_GET['status'] == "erro") {
-        $status_msg = "<p class='msg-erro'>Ocorreu um erro ao enviar sua avaliação.</p>";
+        $status_msg = "<p class='msg-erro'>Ocorreu um erro ao enviar sua dúvida.</p>";
     }
 }
 ?>
@@ -13,8 +13,8 @@ if (isset($_GET['status'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Avaliação IT</title>
-    <link rel="stylesheet" href="../css/avaliação.css">
+    <title>Dúvidas do Cliente</title>
+    <link rel="stylesheet" href="../css/dúvidas.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@400;700">
 </head>
 <body>
@@ -22,13 +22,13 @@ if (isset($_GET['status'])) {
 
 <header class="header">
     <div class="header-container">
-        <h1 class="header-title">Avaliação</h1>
+        <h1 class="header-title">Mande Sua Dúvida !</h1>
         <nav class="header-menu">
             
             <a href="../páginaprincipal.html"><span class="material-symbols-outlined">home</span></a>
             <a href="../Sobre Nós.html"><span class="material-symbols-outlined">info</span></a>
             <a href="../perguntasfrequentes.html"><span class="material-symbols-outlined">support</span></a>
-            <a href="../Projeto-Final-Programa-o-Web/dúvidas/dúvidas.php"><span class="material-symbols-outlined">message</span></a>
+            <a href="../contatos/avaliacao.php"><span class="material-symbols-outlined">star</span></a>
 
         </nav>
     </div>
@@ -36,22 +36,22 @@ if (isset($_GET['status'])) {
 
 
 <main>
-    <h1 class="titulo-principal">Deixe sua avaliação do nosso site!</h1>
+    <h1 class="titulo-principal">Envie suas dúvidas para nós!</h1>
 
     <?= $status_msg ?>
 
     <div class="form-avaliacao">
-        <form action="../valida_avaliacao.php" method="POST">
+        <form action="../valida_duvidas.php" method="POST">
             <label>Nome</label>
             <input type="text" name="nome" required>
 
-            <label>Email</label>
-            <input type="email" name="email" required>
+            <label>Telefone</label>
+            <input type="Número" name="Número" required>
 
-            <label>Avaliação</label>
+            <label>Sua dúvida</label>
             <textarea name="mensagem" rows="6" required></textarea>
 
-            <button type="submit" class="botao-enviar">Enviar Avaliação</button>
+            <button type="submit" class="botao-enviar">Enviar Dúvida</button>
         </form>
     </div>
 </main>
